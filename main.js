346f55f999cb86fa1b50fdb6f8c1f022813d7db1
Cameron Steele <cams@Camerons-MacBook-Pro.local>
2022-03-30 16:55:42 -0500

    Inital Commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const { Client, Server } = require(path.join());
+const midi = require("midi");
+var oscServer = new Server(8010, "0.0.0.0", () => {
+  console.log("OSC Server is listening");
+});
+const client = new Client("127.0.0.1", 8010);
+
+const input = new midi.Input();
+
+// Count the available input ports.
+input.getPortCount();
+
+// Get the name of a specified input port.
+input.getPortName(0);
+input.on("message", (deltaTime, message) => {
+  client.send(`/${message[1]}`, message[2]);
+});
+
+input.openPort(0);
+
+input.ignoreTypes(false, false, false);
